3ad1306d648e934ef04b285a69bb1af89b1998a6
AdamMatulay <adammatulay@proton.me>
2025-10-23 20:17:41 +0200

    learning js

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const submitButton = document.getElementById('submit-button');
-const newsletterCard = document.querySelector('.newsletter-card');
-const newsletterMessage = document.querySelector('.newsletter-confirmation');
-const dismissButton = document.getElementById('dismiss-button');
+const form = document.getElementById('newsletter-email-form');
 
-function subscripeToNewsletter() {
-    newsletterCard.classList.toggle('hidden');
-    newsletterMessage.classList.toggle('hidden');
+// MANUAL FORM VALIDATION
+
+function validateData(data) {
+    if(!data.email.trim()) {
+        showError();
+    } else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
+       showError(); 
+    }
 }
 
-submitButton.addEventListener('click', subscripeToNewsletter);
+// ERROR MESSAGE
+
+function showError() {
+    const errorInput = document.getElementById('email');
+    const errorMessage = document.getElementById('email-error');
+
+    errorInput.classList.add('error');
+    errorMessage.textContent = 'Valid email required';
+}
+
+// FORM SUBMIT FUNCTION
+
+function handleSubmit(e) {
+    e.preventDefault();
+    
+    const data = Object.fromEntries(new FormData(e.target));
+    
+    validateData(data);
+}
+
+form.addEventListener('submit', handleSubmit);
