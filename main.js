b6db65e5844e13c7a3b4cf35df7e967bb0501d99
AdamMatulay <adammatulay@proton.me>
2025-10-24 18:32:26 +0200

    finished js

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
 const form = document.getElementById('newsletter-email-form');
+const emailInput = document.getElementById('email');
+const newsletterConfirmation = document.querySelector('.newsletter-confirmation');
+const dismissMessage = document.getElementById('dismiss-button');
+
 
 // MANUAL FORM VALIDATION
 
 function validateData(data) {
-    if(!data.email.trim()) {
-        showError();
-    } else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
-       showError(); 
+    const errorMessage = document.getElementById('email-error');
+    
+    if(!data.email.trim() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
+        emailInput.classList.add('error');
+        errorMessage.textContent = 'Valid email required';
+        return(false);
+    } else {
+        emailInput.classList.remove('error');
+        errorMessage.textContent = '';
+        return(true);
     }
 }
 
-// ERROR MESSAGE
+// LIVE INPUT VALIDATION
 
-function showError() {
-    const errorInput = document.getElementById('email');
-    const errorMessage = document.getElementById('email-error');
-
-    errorInput.classList.add('error');
-    errorMessage.textContent = 'Valid email required';
-}
+emailInput.addEventListener('input', () => {
+    const data = { email: emailInput.value };
+    validateData(data);
+});
 
 // FORM SUBMIT FUNCTION
 
 function handleSubmit(e) {
     e.preventDefault();
     
-    const data = Object.fromEntries(new FormData(e.target));
-    
-    validateData(data);
+    const data = { email: emailInput.value };
+
+    let result = validateData(data);
+
+    const newsletterCard = document.querySelector('.newsletter-card');
+    const emailInputConfirmation = document.getElementById('email-input-confirmation');
+
+    if (result === true) {
+        newsletterCard.classList.toggle('hidden');
+        newsletterConfirmation.classList.toggle('hidden');
+        emailInputConfirmation.textContent = emailInput.value;
+    } 
 }
 
 form.addEventListener('submit', handleSubmit);
+
+// DISMISS BUTTON FUNCTION
+
+const egg = document.querySelector('.egg');
+
+dismissMessage.addEventListener('click', () => {
+    newsletterConfirmation.classList.toggle('hidden');
+    egg.classList.toggle('hidden');
+});
